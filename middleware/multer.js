f83f71eba3319e8bacf3ed59ed41e85b1d72041a
AnitatInfowind <132639737+AnitatInfowind@users.noreply.github.com>
2024-03-28 11:05:00 +0530

    multer changes

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,9 +3,6 @@ const path = require("path");
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: async (req, file, cb) => {
-      cb(null, path.join(__dirname, "../uploads"));
-    },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       cb(
